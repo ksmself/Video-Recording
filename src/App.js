f12892144ce67dcc10485e4954c42946d8ad8e13
ksmself <53217988+ksmself@users.noreply.github.com>
2021-02-19 19:34:51 +0900

    cra

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
 
-function App() {
+const App = () => {
+  const recorder = document.getElementById("recorder");
+  const player = document.getElementById("player");
+
+  // recorder.addEventListener("change", function (e) {
+  //   const file = e.target.files[0];
+  //   player.src = URL.createObjectURL(file);
+  // });
+
+  const handleSuccess = function (stream) {
+    player.srcObject = stream;
+  };
+
+  navigator.mediaDevices.enumerateDevices().then((devices) => {
+    devices = devices.filter((d) => d.kind === "videoinput");
+  });
+
+  navigator.mediaDevices
+    .getUserMedia({ audio: true, video: true })
+    .then(handleSuccess);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CameraInput>
+      <input type="file" accept="video/*" capture="camera" id="recorder" />
+      <video id="player" controls></video>
+    </CameraInput>
   );
-}
+};
+
+const CameraInput = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  transform: translateY(-10%);
+`;
 
 export default App;
